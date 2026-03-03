c6f2345f7462e7757853ec1bf781e6e121474e2a
hiteshkumar54 <hitesh.gla_cs.aiml24@gla.ac.in>
2026-03-03 11:13:29 +0530

    Update model.py

--- ml/model.py
+++ ml/model.py
@@ -2,8 +2,8 @@ import numpy as np
 from sklearn.linear_model import LinearRegression
 
 # Sample historical cloud usage data
-usage = np.array([[100], [200], [300], [400], [500]])
-cost = np.array([1000, 2000, 3000, 4000, 5000])
+usage = np.array([[100], [200], [300], [400], [500], [600]])
+cost = np.array([1100, 1900, 3200, 4100, 5300, 6100])
 
 # Train model
 model = LinearRegression()
